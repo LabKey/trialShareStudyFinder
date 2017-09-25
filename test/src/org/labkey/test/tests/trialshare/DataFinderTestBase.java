ab1610854e4093f2a92dd7f5cdd1a2b946ea0a3a
chrisj <chrisj@labkey.com>
2017-09-25 12:57:03 -0700

    fix whitelisted test to use admin page helper

--- test/src/org/labkey/test/tests/trialshare/DataFinderTestBase.java
+++ test/src/org/labkey/test/tests/trialshare/DataFinderTestBase.java
@@ -214,7 +214,7 @@ public abstract class DataFinderTestBase extends BaseWebDriverTest
     protected void reindexForSearch()
     {
         log("Reindexing data for full-text search");
-        goToAdminConsole();
+        goToAdminConsole().goToAdminConsoleLinksSection();
         clickAndWait(Locator.linkWithText("Data Cube"));
         clickButton("Reindex", 0);
         clickButton("OK");
