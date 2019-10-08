ab4775f3baa9fe4cfc3a3b2f71db1f18ed9d891a
labkey-tchad <tchad@labkey.com>
2019-10-08 15:06:02 -0700

    Tests for ITN specimen behavior

--- test/src/org/labkey/test/tests/trialshare/ITNSpecimenTest.java
+++ test/src/org/labkey/test/tests/trialshare/ITNSpecimenTest.java
@@ -1,5 +1,6 @@
 package org.labkey.test.tests.trialshare;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,15 +13,22 @@ import org.labkey.test.TestTimeoutException;
 import org.labkey.test.categories.Git;
 import org.labkey.test.components.study.SpecimenDetailGrid;
 import org.labkey.test.pages.study.samples.ManageRequestPage;
+import org.labkey.test.pages.study.samples.ManageRequestStatusPage;
 import org.labkey.test.pages.study.samples.ShowCreateSampleRequestPage;
+import org.labkey.test.util.ApiPermissionsHelper;
 import org.labkey.test.util.ExperimentalFeaturesHelper;
 import org.labkey.test.util.LogMethod;
+import org.labkey.test.util.PermissionsHelper.MemberType;
 import org.labkey.test.util.TestLogger;
 import org.labkey.test.util.study.specimen.SpecimenHelper;
+import org.openqa.selenium.support.ui.UnexpectedTagNameException;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
 
 @Category({Git.class})
 public class ITNSpecimenTest extends BaseWebDriverTest
@@ -28,11 +36,15 @@ public class ITNSpecimenTest extends BaseWebDriverTest
     private static final File STUDY_ARCHIVE = TestFileUtils.getSampleData("studies/LabkeyDemoStudy.zip");
     private static final String ITN_SPECIMEN_HANDLING = "ITNSpecimenHandling";
 
+    private static final String SPECIMEN_COORDINATOR = "specimen_coordinator@itnspecimen.test";
+    private static final String MAGIC_GROUP = "Specimen Request Administrators";
+
     @Override
     protected void doCleanup(boolean afterTest) throws TestTimeoutException
     {
         super.doCleanup(afterTest);
         enableITNSpecimenHandling(false);
+        _userHelper.deleteUsers(false, SPECIMEN_COORDINATOR);
     }
 
     @BeforeClass
@@ -47,6 +59,7 @@ public class ITNSpecimenTest extends BaseWebDriverTest
     {
         _containerHelper.createProject(getProjectName(), "Study");
         importStudyFromZip(STUDY_ARCHIVE);
+        new SpecimenHelper(this).setupRequestStatuses();
     }
 
     @Before
@@ -63,14 +76,13 @@ public class ITNSpecimenTest extends BaseWebDriverTest
 
         // TODO: type breakdown select is missing
 
-        // TODO: Report looks like 'Primary Type' report
+        // TODO: Report looks like 'Derivative Type' report
     }
 
     @Test
     public void testSpecimenRequestCustomization()
     {
         SpecimenHelper specimenHelper = new SpecimenHelper(this);
-        specimenHelper.setupRequestStatuses();
         clickPortalTab("Specimen Data");
 
         clickAndWait(Locator.linkWithText("Urine"));
@@ -78,12 +90,16 @@ public class ITNSpecimenTest extends BaseWebDriverTest
         specimenGrid.checkCheckbox(0);
         ShowCreateSampleRequestPage requestPage = specimenGrid.createNewRequest();
 
+        // TODO: Refactor to use SampleManagementErrorLogger after it has been moved out of sampleManagement
         TestLogger.log("\"Reqeusting Location\" should not be available for ITN specimen requests");
         assertElementNotVisible(Locator.id("destinationLocation"));
 
         ManageRequestPage manageRequestPage = requestPage
                 .setDetails("plan", "info") // required fields
                 .clickCreateAndViewDetails();
+
+        String requestingLocation = manageRequestPage.getRequestInformation("Requesting Location");
+        assertEquals("Wrong 'Requesting Location'.", "User Request", requestingLocation);
 
         specimenGrid = specimenHelper.findSpecimenDetailGrid();
         specimenGrid.checkCheckbox(0);
@@ -99,9 +115,56 @@ public class ITNSpecimenTest extends BaseWebDriverTest
     }
 
     @Test
-    public void testRequestListCustomization()
+    public void testRequestPermissionCustomization()
     {
+        ApiPermissionsHelper permissions = new ApiPermissionsHelper(this);
 
+        _userHelper.createUser(SPECIMEN_COORDINATOR);
+        permissions.addMemberToRole(SPECIMEN_COORDINATOR, "Specimen Coordinator", MemberType.user, getProjectName());
+        permissions.addMemberToRole(SPECIMEN_COORDINATOR, "Reader", MemberType.user, getProjectName());
+        permissions.createGlobalPermissionsGroup(MAGIC_GROUP);
+
+        ManageRequestStatusPage manageRequestStatusPage = ShowCreateSampleRequestPage.beginAt(this, getProjectName())
+                .setDetails("A", "B")
+                .clickCreateAndViewDetails()
+                .submitRequest()
+                .clickUpdateRequest();
+
+        TestLogger.log("Site admin should be able to set request status");
+        manageRequestStatusPage
+                .setStatus("Processing")
+                .clickSave();
+
+        int id = Integer.parseInt(getUrlParam("id"));
+
+        impersonate(SPECIMEN_COORDINATOR);
+        {
+            TestLogger.log("Need to be in special group to update requrest status");
+            manageRequestStatusPage = ManageRequestStatusPage.beginAt(this, getProjectName(), id);
+            try
+            {
+                manageRequestStatusPage.getStatus();
+                Assert.fail("Only users in the correct group should see 'status' dropdown");
+            }
+            catch (NoSuchElementException | UnexpectedTagNameException ignore) { /* expected exception */ }
+
+            String readOnlyStatus = Locator.input("status").parent().findElement(getDriver()).getText();
+            assertEquals("Should see status", "Processing", readOnlyStatus);
+        }
+        stopImpersonating();
+
+        permissions.addUserToSiteGroup(SPECIMEN_COORDINATOR, MAGIC_GROUP);
+
+        impersonate(SPECIMEN_COORDINATOR);
+        {
+            ManageRequestStatusPage.beginAt(this, getProjectName(), id)
+                    .setStatus("Completed")
+                    .clickSave();
+        }
+        stopImpersonating();
+
+        String status = ManageRequestPage.beginAt(this, getProjectName(), id).getRequestInformation("Status");
+        assertEquals("Final Status", "Completed", status);
     }
 
     @LogMethod(quiet = true)
