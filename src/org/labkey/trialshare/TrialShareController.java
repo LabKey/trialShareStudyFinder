69fadf5609f82cd8a0425ddce758b058aecaa92c
klum <klum@labkey.com>
2020-11-16 12:07:34 -0800

    exclude Notebooks from the trial share export test

--- src/org/labkey/trialshare/TrialShareController.java
+++ src/org/labkey/trialshare/TrialShareController.java
@@ -32,6 +32,7 @@ import org.labkey.api.admin.FolderWriter;
 import org.labkey.api.admin.FolderWriterImpl;
 import org.labkey.api.admin.StaticLoggerGetter;
 import org.labkey.api.data.Container;
+import org.labkey.api.data.ContainerManager;
 import org.labkey.api.data.PHI;
 import org.labkey.api.pipeline.PipeRoot;
 import org.labkey.api.pipeline.PipelineService;
@@ -571,7 +572,9 @@ public class TrialShareController extends SpringActionController
 
     public static class TrialShareExportTest
     {
-        private final Collection<FolderWriter> folderWriters = FolderSerializationRegistry.get().getRegisteredFolderWriters();
+        private final Collection<FolderWriter> folderWriters = FolderSerializationRegistry.get().getRegisteredFolderWriters().stream()
+                .filter(fw -> !"Notebooks".equals(fw.getDataType()))
+                .collect(Collectors.toList());
 
         @Test
         public void testDataTypes()
